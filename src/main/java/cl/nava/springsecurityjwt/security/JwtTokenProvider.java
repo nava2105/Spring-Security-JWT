2c0ed7b5ca8c200c4b4ccc0918aa995a416f0b79
Nava <manosalvas.mateof@gmail.com>
2024-02-03 18:06:54 -0500

    Change the spring version from 3.2.2 to 3.0.6, and create a class with our security constants, and another class to create, extract and validate our jwt token

---
+++ src/main/java/cl/nava/springsecurityjwt/security/JwtTokenProvider.java
@@ -0,0 +1,49 @@
+package cl.nava.springsecurityjwt.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    //Metodo para crear un token por medio de la autenticación
+    public String generarToken(Authentication authentication){
+        String userName = authentication.getName();
+        Date tiempoActual = new Date();
+        Date expiracionToken = new Date(tiempoActual.getTime() + ConstantesSeguridad.JWT_EXPIRATION_TOKEN);
+
+        // Linea para generar el token
+        String token = Jwts.builder()
+                .setSubject(userName)
+                .setIssuedAt(new Date())
+                .setExpiration(expiracionToken)
+                .signWith(SignatureAlgorithm.HS512, ConstantesSeguridad.JWT_FIRMA)
+                .compact();
+        return token;
+    }
+
+    //Metodo para extraer un username a pàrtir de un token
+    public String obtenerUsernameDeJwt(String token){
+        Claims claims= Jwts.parser()
+                .setSigningKey(ConstantesSeguridad.JWT_FIRMA)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    //Metodo para validar el token
+    public boolean validarToken(String token){
+        try {
+            Jwts.parser().setSigningKey(ConstantesSeguridad.JWT_FIRMA).parseClaimsJws(token);
+            return true;
+        }catch (Exception e){
+            throw new AuthenticationCredentialsNotFoundException("Jwt ha expirado o esta incorrecto");
+        }
+    }
+}
