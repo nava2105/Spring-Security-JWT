9b003dfd30583bfe468e963d1659b64d67ef2f66
nava2105 <150807583+nava2105@users.noreply.github.com>
2025-01-09 11:08:03 -0500

    feat(🔨 Add initializers): Create an initializer that creates the roles if they don't exist when the app starts

---
+++ src/main/java/cl/nava/springsecurityjwt/initializers/RolesInitializer.java
@@ -0,0 +1,36 @@
+package cl.nava.springsecurityjwt.initializers;
+
+import cl.nava.springsecurityjwt.models.Roles;
+import cl.nava.springsecurityjwt.repositories.IRolesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RolesInitializer implements CommandLineRunner {
+
+    private IRolesRepository rolesRepository;
+
+    @Autowired
+    public RolesInitializer(IRolesRepository rolesRepository) {
+        this.rolesRepository = rolesRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Check if the role "ADMIN" exists, if not, it is created.
+        if (rolesRepository.findByName("ADMIN").isEmpty()) {
+            Roles adminRole = new Roles();
+            adminRole.setName("ADMIN");
+            rolesRepository.save(adminRole);
+            System.out.println("Role 'ADMIN' created.");
+        }
+        // Check if the role "USER" exists, if not, it is created.
+        if (rolesRepository.findByName("USER").isEmpty()) {
+            Roles userRole = new Roles();
+            userRole.setName("USER");
+            rolesRepository.save(userRole);
+            System.out.println("Role 'USER' created.");
+        }
+    }
+}
